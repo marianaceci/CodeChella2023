f4f5518eb7336df4d1ad55c61ebfb6146758a84c
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-03-09 19:03:10 -0300

    testando lista dropdown

--- src/pages/SeuIngresso/Form/index.jsx
+++ src/pages/SeuIngresso/Form/index.jsx
@@ -1,31 +1,11 @@
-import { useState } from 'react';
 import ButtonSubmit from '../../../components/ButtonSubmit';
+import TipoDeIngresso from './dropdown';
 import styles from './Form.module.scss';
 
-const Form = ({ onSubmit }) => {
-  const safeSubmit = event => {
-    event.preventDefault()
-    event.stopPropagation()
-    const nome = event.target[0].value
-    const email = event.target[1].value
-    const tipo = event.target[2].value
-    const nascimento = event.target[3].value
-    onSubmit({nome, email, tipo, nascimento})
-  }
-
-  const tiposDeIngresso = [
-    {label: 'Ingresso Cortesia', value:'ingressoCortesia'},
-    {label: 'Ingresso Inteiro', value:'ingressoInteiro'},
-    {label: 'Ingresso meia-entrada', value:'ingressoMeia'},
-  ]
-
-  const [ingresso, setIngresso] = useState('Tipo de ingresso')
-  const handleChange = event => {
-    setIngresso(event.target.value)
-  }
+const Form = () => {
 
   return (
-    <form onSubmit={safeSubmit} className={styles.formulario}>
+    <form className={styles.formulario}>
       <div className={styles.formulario__campo}>
         <label htmlFor="nome">Nome Completo:</label>
         <input type="text" />
@@ -37,7 +17,7 @@ const Form = ({ onSubmit }) => {
       <div className={styles.select}>
         <div className={styles.formulario__campo}>
           <label htmlFor="tipo">Tipo de ingresso</label>
-          <input type="text" />
+          <TipoDeIngresso />
         </div>
         <div className={styles.formulario__campo}>
           <label htmlFor="nascimento">Data de nascimento:</label>
