b67eae852af585e1303725c67027f51ca5efe6f6
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-03-30 20:27:48 -0300

    ajuste de path

--- src/data/bannerInfo.js
+++ src/data/bannerInfo.js
@@ -2,27 +2,27 @@ export const bannerInfo = [
   {
     pagina: "experiencia",
     titulo: "A Experiência",
-    src: '/banners/experiencia-banner.png'
+    src:'/banners/experiencia-banner.png'
   },
   {
     pagina: "setores",
     titulo: "Mapa de Setores",
-    src: '/banners/setores-banner.png'
+    src:'/banners/setores-banner.png'
   },
   {
     pagina: "informacoes",
     titulo: "Informações Gerais",
-    src: '/banners/informacoes-banner.png'
+    src:'/banners/informacoes-banner.png'
   },
   {
     pagina: "ingresso",
     titulo: "Garanta seu Ingresso",
-    src: '/banners/ingresso-banner.png'
+    src:'/banners/ingresso-banner.png'
   },
   {
     pagina: "qringresso",
     titulo: "Seu ingresso está aqui!",
-    src: '/banners/qringresso-banner.png'
+    src:'/banners/qringresso-banner.png'
   }
 ];
 
