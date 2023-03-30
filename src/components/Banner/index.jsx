4ddc687aee9f9acc6eba12f12fe2aec948b6b9a2
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-03-30 20:13:30 -0300

    ajuste de path

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,7 +1,6 @@
 import { BannerSection } from "./style";
 import { bannerInfo } from "@/data/bannerInfo";
 import { useTheme } from "styled-components";
-import { getBanners } from "../../data/bannerInfo";
 
 export default function Banner({blendmode}) {
   const theme = useTheme();
@@ -11,9 +10,8 @@ export default function Banner({blendmode}) {
   const path = originalPath.slice(22);
   const isMain = path === '';
 
-  const bannerInfo = getBanners();
   const bannerSelected = bannerInfo.filter((item) => item.pagina === path);
-  const imageBanner = isMain ? bannerTheme : bannerSelected[0].bannersrc;
+  const imageBanner = isMain ? bannerTheme : bannerSelected[0].src;
 
   return (
     <BannerSection>
--- src/data/bannerInfo.js
+++ src/data/bannerInfo.js
@@ -1,35 +1,35 @@
-const bannerInfo = [  
+export const bannerInfo = [  
   {
     pagina: "",
     titulo: "Boas-vindas ao #CodeChella2023!",
-    bannersrc: '/banners/boreal-home-banner.png'
+    src: '/banners/boreal-home-banner.png'
   },
   {
     pagina: "experiencia",
     titulo: "A Experiência",
-    bannersrc: '/banners/experiencia-banner.png'
+    src: '/banners/experiencia-banner.png'
   },
   {
     pagina: "setores",
     titulo: "Mapa de Setores",
-    bannersrc: '/banners/setores-banner.png'
+    src: '/banners/setores-banner.png'
   },
   {
     pagina: "informacoes",
     titulo: "Informações Gerais",
-    bannersrc: '/banners/informacoes-banner.png'
+    src: '/banners/informacoes-banner.png'
   },
   {
     pagina: "ingresso",
     titulo: "Garanta seu Ingresso",
-    bannersrc: '/banners/ingresso-banner.png'
+    src: '/banners/ingresso-banner.png'
   },
   {
     pagina: "qringresso",
     titulo: "Seu ingresso está aqui!",
-    bannersrc: '/banners/qringresso-banner.png'
+    src: '/banners/qringresso-banner.png'
   }
 ];
 
-export const getBanners = () => bannerInfo;
+
 
