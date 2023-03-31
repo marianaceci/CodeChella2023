c6123fc35af2ded818edd9a18b789e04411dfc82
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2023-03-31 16:56:43 -0300

    atualizando btao tema

--- src/styles/botaoSwitcher.js
+++ src/styles/botaoSwitcher.js
@@ -2,13 +2,13 @@ import { variaveis } from "./variaveis";
 import styled from "styled-components";
 
 export const BotaoTema = styled.button`
-  position:absolute;
+  position: absolute;
   top: 1vh;
   left: 10px;
   background-color: inherit;
   border: none;
   cursor: pointer;
-  z-index: 10;
+  z-index: 20;
   color: ${variaveis.summer.branco};
   svg{
     height: 25px;
